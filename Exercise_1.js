dfff550c7412168ed63b085222eec508fbd7e7a1
Your Name <you@example.com>
2019-06-21 08:48:29 +0200

    first

---
+++ Exercise_1.js
@@ -0,0 +1,30 @@
+ 
+    //====================
+    var a = loopUntil(6);
+    //console.log(a);
+var check = false;
+    function loopUntil(x){
+            var random;
+            var valArray;
+        var arrCheck = [];
+var a = 1;
+var b = 2;
+if(x >= 0 && x <= 9){
+while(a<b){
+        random = Math.round(Math.random()*10);
+               for(var i = 0 ; i < 3; i++){
+                arrCheck[i] = random;
+                console.log(arrCheck);
+                if (arrCheck[i] == arrCheck[i +1] && arrCheck[i] == arrCheck [i +2]){
+                        console.log("match" + arrCheck);
+                        a = 3;
+                }
+        }
+      // console.log(arrCheck);
+}
+}
+        }
+
+
+
+        
