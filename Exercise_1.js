e8c93f2ff41f947f955a11214779fded905932f2
Your Name <you@example.com>
2019-06-21 09:02:00 +0200

    edit

--- Exercise_1.js
+++ Exercise_1.js
@@ -1,6 +1,6 @@
  
     //====================
-    var a = loopUntil(6);
+    var a = loopUntil(3);
     //console.log(a);
 var check = false;
     function loopUntil(x){
@@ -12,15 +12,20 @@ var b = 2;
 if(x >= 0 && x <= 9){
 while(a<b){
         random = Math.round(Math.random()*10);
+        
                for(var i = 0 ; i < 3; i++){
+        
                 arrCheck[i] = random;
                 console.log(arrCheck);
                 if (arrCheck[i] == arrCheck[i +1] && arrCheck[i] == arrCheck [i +2]){
+                        if(arrCheck[i] > x){
                         console.log("match" + arrCheck);
                         a = 3;
                 }
         }
+        }
       // console.log(arrCheck);
+      console.log(valArray);
 }
 }
         }
